47c313d8642f42e4b14c2ba10a624022a37b2c4a
zjz236 <zjz236@qq.com>
2020-01-31 20:30:27 +0800

    feat: 添加test.py

---
+++ app/source/test.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python
+#! -*- coding: utf8 -*-
+
+import lorun
+import os
+
+RESULT_STR = [
+    'Accepted',
+    'Presentation Error',
+    'Time Limit Exceeded',
+    'Memory Limit Exceeded',
+    'Wrong Answer',
+    'Runtime Error',
+    'Output Limit Exceeded',
+    'Compile Error',
+    'System Error'
+]
+
+def compileSrc(src_path):
+    if os.system('gcc %s -o m'%src_path) != 0:
+        print('compile failure!')
+        return False
+    return True
+
+def runone(p_path, in_path, out_path):
+    fin = open(in_path)
+    ftemp = open('temp.out', 'w')
+
+    runcfg = {
+        'args':[p_path],
+        'fd_in':fin.fileno(),
+        'fd_out':ftemp.fileno(),
+        'timelimit':1000, #in MS
+        'memorylimit':20000, #in KB
+    }
+
+    rst = lorun.run(runcfg)
+    fin.close()
+    ftemp.close()
+
+    if rst['result'] == 0:
+        ftemp = open('temp.out')
+        fout = open(out_path)
+        crst = lorun.check(fout.fileno(), ftemp.fileno())
+        fout.close()
+        ftemp.close()
+        os.remove('temp.out')
+        if crst != 0:
+            return {'result':crst}
+
+    return rst
+
+def judge(src_path, td_path, td_in):
+        in_path = os.path.join('../public/', td_path)
+        out_path = os.path.join('../public/', td_in)
+        if os.path.isfile(in_path) and os.path.isfile(out_path):
+            rst = runone(src_path, in_path, out_path)
+            rst['result'] = RESULT_STR[rst['result']]
+            print(rst)
+        else:
+            print('testdata:%d incompleted' % i)
+            exit(-1)
+
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) != 4:
+        print('Usage:%s srcfile testdata_pth testdata_total')
+        exit(-1)
+    judge(sys.argv[1], sys.argv[2], sys.argv[3]))
