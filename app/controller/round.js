84ee139c19fce99a0d9ea9c90435ce47aaf892da
zjz236 <zjz236@qq.com>
2020-02-01 12:26:22 +0800

    测试test.py

--- app/controller/round.js
+++ app/controller/round.js
@@ -179,7 +179,7 @@ class downloadController extends Controller {
         }
       })
       fs.writeFileSync(path.join(__dirname, '../public/outputData/' + fileName + '.out'), runnerResult.output)
-      const child = shell.exec(`python ${path.join(__dirname, '../source/test.py')} ${runShell} ${inputFile} outputData/${fileName}.out`, {
+      const child = shell.exec(`python3 ${path.join(__dirname, '../source/test.py')} ${runShell} ${inputFile} outputData/${fileName}.out`, {
         silent: true,
         timeout: 10000
       })
