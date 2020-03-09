9c110069683cf7f28eba98b2d4a626bea06c3243
zjz236 <zjz236@qq.com>
2020-03-09 21:14:06 +0800

    修改token时效

--- app/controller/account.js
+++ app/controller/account.js
@@ -17,7 +17,7 @@ class accountController extends Controller {
         }
       })
       if (result && result.username) {
-        const token = loginToken({ username: result.username, userId: result._id }, 7200)
+        const token = loginToken({ username: result.username, userId: result._id }, 24 * 60)
         await mongo.deleteMany('loginToken', {
           filter: {
             userId: result._id
